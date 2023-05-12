ef0ccf6d1e6b0276016d58348a51987a316a1b98
Bryan <52650733+xbyubryan@users.noreply.github.com>
2023-05-12 17:48:17 +0800

    百度统计完整版

--- pages/application/application.vue
+++ pages/application/application.vue
@@ -1,12 +1,3 @@
-<script>
-	var _hmt = _hmt || [];
-	(function() {
-		var hm = document.createElement("script");
-		hm.src = "https://hm.baidu.com/hm.js?720f3b0028584f4587d46b433db6aaf1";
-		var s = document.getElementsByTagName("script")[0];
-		s.parentNode.insertBefore(hm, s);
-	})();
-</script>
 <template>
 	<view class="content">
 		<img class="bgImg" src="/static/images/jump_head.png" alt="">
